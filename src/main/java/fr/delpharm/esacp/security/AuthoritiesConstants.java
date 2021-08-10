a172ad5ff34618cc3c85ad766f51de29289330d8
GSoulat <gsoulat31@gmail.com>
2021-08-10 14:25:14 +0200

    Mise à jour de la BDD (connection + data+relationship) + rajout des roles

--- src/main/java/fr/delpharm/esacp/security/AuthoritiesConstants.java
+++ src/main/java/fr/delpharm/esacp/security/AuthoritiesConstants.java
@@ -11,5 +11,15 @@ public final class AuthoritiesConstants {
 
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
+    public static final String REDACTEUR = "ROLE_REDACTEUR";
+
+    public static final String PILOTE = "ROLE_PILOTE";
+
+    public static final String PORTEUR = "ROLE_PORTEUR";
+
+    public static final String HSE = "ROLE_HSE";
+
+    public static final String V_HSE = "ROLE_V_HSE";
+
     private AuthoritiesConstants() {}
 }
